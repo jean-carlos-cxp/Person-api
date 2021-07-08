6bcbe3fcb652e0c96402bbf84d934058885b3d66
Jean Carlos <carlosjean090@gmail.com>
2021-07-08 14:52:26 -0300

    H2 database, test profile, JPA

--- src/main/java/one/digitalinovation/personapi/entities/Person.java
+++ src/main/java/one/digitalinovation/personapi/entities/Person.java
@@ -4,14 +4,30 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+    @Column(nullable = false)
 	private String firstName;
-	private String lastName;
-	private String cpf;
+    
+    @Column(nullable = false)
+    private String lastName;
+    
+    @Column(nullable = false)
+    private String cpf;
+    
 	private Date birthDate;
 	
 	public Person() {
